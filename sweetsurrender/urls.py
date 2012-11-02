c5f6c9c8675af517f9272c5bede766204f7414e6
albertgeorge <albert.george@outlook.com>
2012-11-02 18:06:04 +0530

    implemented basehtml template

--- sweetsurrender/urls.py
+++ sweetsurrender/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sweetsurrender.views.home', name='home'),
+    url(r'^sweetsurrender/$', 'sweetsurrender.views.home'),
     # url(r'^sweetsurrender/', include('sweetsurrender.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -15,3 +17,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
